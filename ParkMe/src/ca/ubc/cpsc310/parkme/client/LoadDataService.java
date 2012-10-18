cb3709ef0f507e990b69ff8f041a99aa51d28393
Frances <Frances@206.87.126.179>
2012-10-17 20:21:08 -0700

    Trying to merge Alyanna's changes

--- ParkMe/src/ca/ubc/cpsc310/parkme/client/LoadDataService.java
+++ ParkMe/src/ca/ubc/cpsc310/parkme/client/LoadDataService.java
@@ -1,5 +1,7 @@
 package ca.ubc.cpsc310.parkme.client;
 
+import ca.ubc.cpsc310.parkme.server.ParkingLoc;
+
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
@@ -7,5 +9,5 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 public interface LoadDataService  extends RemoteService{
 	public void loadData();
 //	public void parseData();
-	public String[] getParking();
+	public ParkingLocation[] getParking();
 }
