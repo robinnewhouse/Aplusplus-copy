8817ab708f6a411d57ccaa3949d9a609b737dc45
Bruce Krayenhoff <wbrucek@gmail.com>
2012-11-15 16:49:06 -0800

    searchstring being committed

--- ParkMe/src/ca/ubc/cpsc310/parkme/server/SearchString.java
+++ ParkMe/src/ca/ubc/cpsc310/parkme/server/SearchString.java
@@ -14,17 +14,60 @@ import com.google.appengine.api.users.User;
 @PersistenceCapable(identityType = IdentityType.APPLICATION)
 public class SearchString {
 	@PrimaryKey
+	@Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+	private Long id;
 	@Persistent
-	String searchString;
+	private String searchString;
 	@Persistent
-	User user;
+	private User user;
+	@Persistent
+	private Date createDate;
+
 	
 	public SearchString(User user, String searchString){
 		this.user=user;
-		this.searchString=searchString;		
+		this.searchString=searchString;	
+		this.createDate = new Date();
 	}
-	
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
 	public String getSearchString() {
 		return searchString;
 	}
+
+
+	public void setSearchString(String searchString) {
+		this.searchString = searchString;
+	}
+
+
+	public User getUser() {
+		return user;
+	}
+
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	
 }
