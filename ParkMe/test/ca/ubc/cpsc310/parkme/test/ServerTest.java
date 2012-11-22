71597232dd4a995fb9796d4d948eadb62557f0c6
Robin Newhouse <robinnewhouse@gmail.com>
2012-11-22 11:03:25 -0800

    even more testing

--- ParkMe/test/ca/ubc/cpsc310/parkme/test/ServerTest.java
+++ ParkMe/test/ca/ubc/cpsc310/parkme/test/ServerTest.java
@@ -1,5 +1,7 @@
 package ca.ubc.cpsc310.parkme.test;
 
+import java.util.List;
+
 import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
@@ -8,22 +10,58 @@ import junit.framework.TestCase;
 
 import org.junit.Test;
 
+import ca.ubc.cpsc310.parkme.client.services.parking.TicketInfo;
+import ca.ubc.cpsc310.parkme.server.Ticket;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.users.User;
+
 public class ServerTest extends TestCase {
 
 	private static final PersistenceManagerFactory PMF = JDOHelper
 			.getPersistenceManagerFactory("transactions-optional");
 	PersistenceManager pm = PMF.getPersistenceManager();
 
-	public void testAddTickets() {
-		// TicketServiceImpl tsi = new TicketServiceImpl();
-		// TicketInfo ti = new TicketInfo();
-		// final TicketServiceAsync ticketService = GWT
-		// .create(TicketService.class);
-		// try {
-		// pm.addTicket("id-1", 100.0);
-		// } catch (NotLoggedInException e) {
-		// e.printStackTrace();
-		// }
+	private void testAddTickets() {
+
+		final PersistenceManagerFactory PMF = JDOHelper
+				.getPersistenceManagerFactory("transactions-optional");
+		PersistenceManager pm = PMF.getPersistenceManager();
+
+		try {
+			setUp();
+			DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+
+			User user1 = new User("user1", "gmail.com");
+			User user2 = new User("user2", "gmail.com");
+			Ticket testTicket1 = new Ticket(user1, "id1", 100.0);
+			Ticket testTicket2 = new Ticket(user1, "id2", 100.0);
+			Ticket testTicket3 = new Ticket(user2, "id3", 100.0);
+
+			TicketInfo[] tickets;
+			try {
+				javax.jdo.Query q = pm.newQuery(Ticket.class, "user == u");
+				q.declareParameters("com.google.appengine.api.users.User u");
+				q.setOrdering("createDate");
+				List<Ticket> pTicket = (List<Ticket>) q.execute(user1);
+
+				int size = pTicket.size();
+				tickets = new TicketInfo[size];
+				for (int i = 0; i < size; i++) {
+					tickets[i] = new TicketInfo(pTicket.get(i).getParkingID(),
+							pTicket.get(i).getFine());
+				}
+			} finally {
+				pm.close();
+			}
+
+			tearDown();
+		} catch (Exception e) {
+			e.printStackTrace();
+			assertTrue(false);
+		}
+
 	}
 
 	@Test
